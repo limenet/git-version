0ebc89b2c4e95ea178d29a7d93bc40c1b8de0f98
Linus Metzler <linus.metzler@gmail.com>
2017-05-07 15:49:59 +0200

    use temp dir

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,3 @@
 <?php
 
 require_once __DIR__.'/../vendor/autoload.php';
-
-define('TEMPDIR', realpath(__DIR__.'/..').'/build-test');
-@mkdir(TEMPDIR);
--- tests/limenet/GitVersion/GitHelper.php
+++ tests/limenet/GitVersion/GitHelper.php
@@ -16,16 +16,17 @@ class GitHelper
            2 => ['pipe', 'w'],  // stderr
         ];
 
-        $process = proc_open('git clone '.$repo.' '.$dir, $descriptorspec, $pipes, TEMPDIR);
+        $process = proc_open('git clone '.$repo.' '.$dir, $descriptorspec, $pipes, sys_get_temp_dir());
 
         fclose($pipes[0]);
         fclose($pipes[1]);
         fclose($pipes[2]);
 
+
         // It is important that you close any pipes before calling
         // proc_close in order to avoid a deadlock
         proc_close($process);
 
-        return TEMPDIR.'/'.$dir;
+        return sys_get_temp_dir().'/'.$dir;
     }
 }
