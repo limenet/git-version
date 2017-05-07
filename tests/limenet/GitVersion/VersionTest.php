f87bc23668c8e58e60346ccf95741dab4e3f1cf8
Linus Metzler <linus.metzler@gmail.com>
2017-05-07 15:36:53 +0200

    validate semver

--- tests/limenet/GitVersion/VersionTest.php
+++ tests/limenet/GitVersion/VersionTest.php
@@ -1,10 +1,12 @@
 <?php
+namespace limenet\GitVersion;
 
 use PHPUnit\Framework\TestCase;
 use limenet\GitVersion\GitHelper;
 use Ramsey\Uuid\Uuid;
 use limenet\GitVersion\Version;
 use limenet\GitVersion\Formatters\SemverFormatter;
+use Composer\Semver\VersionParser;
 
 class VersionTest extends TestCase
 {
@@ -14,9 +16,11 @@ class VersionTest extends TestCase
 
         $version = new Version($dir);
         $v = $version->get(new SemverFormatter());
+        $vv = (new VersionParser())->normalize($v);
 
-        var_dump($v);
+        var_dump($v,$vv);
         $this->assertNotEmpty($v);
+        $this->assertInternalType('string', $v);
     }
 
     public function testMissingFormatter() : void
