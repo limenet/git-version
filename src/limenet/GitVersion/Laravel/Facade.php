50349f0ee27b19d3377a9e8bdc9293ea63c7f02d
Linus Metzler <hi@linusmetzler.me>
2018-02-02 09:07:15 +0100

    added Laravel Facade and Service Provider

---
+++ src/limenet/GitVersion/Laravel/Facade.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace limenet\GitVersion\Laravel;
+use Illuminate\Support\Facades\Facade as BaseFacade;
+class Facade extends BaseFacade
+{
+    protected static function getFacadeAccessor()
+    {
+        return 'git-version';
+    }
+}
---
+++ src/limenet/GitVersion/Laravel/ServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace limenet\GitVersion\Laravel;
+
+use Illuminate\Support\ServiceProvider as BaseProvider;
+use limenet\GitVersion\Directory;
+use limenet\GitVersion\Formatters\SemverFormatter;
+
+class ServiceProvider extends BaseProvider
+{
+    public function provides()
+    {
+        return ['git-version'];
+    }
+    public static function get()
+    {
+        return (new Directory(base_path()))->get(new SemverFormatter());
+    }
+}
