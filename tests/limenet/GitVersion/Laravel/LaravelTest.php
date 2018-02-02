b8df7f669df18a2dd48fc00f7c48af870bf5b8ce
Linus Metzler <hi@linusmetzler.me>
2018-02-02 08:47:51 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- tests/limenet/GitVersion/Laravel/LaravelTest.php
+++ tests/limenet/GitVersion/Laravel/LaravelTest.php
@@ -2,30 +2,29 @@
 
 namespace limenet\GitVersion\Laravel;
 
-use limenet\GitVersion\Formatters\SemverFormatter;
-use limenet\GitVersion\Formatters\UrlFormatter;
 use Orchestra\Testbench\TestCase;
-use limenet\GitVersion\Laravel\Facade;
-use limenet\GitVersion\Laravel\ServiceProvider;
 
-class LaravelTest extends TestCase {
-
+class LaravelTest extends TestCase
+{
     protected function getPackageProviders($app)
     {
         return [ServiceProvider::class];
     }
+
     protected function getPackageAliases($app)
     {
         return [
-            'GitVersion' => Facade::class
+            'GitVersion' => Facade::class,
         ];
     }
-public function testIsBound()
+
+    public function testIsBound()
     {
         $this->assertArrayHasKey(ServiceProvider::class, app()->getLoadedProviders());
     }
 
-    public function testFacade(){
+    public function testFacade()
+    {
         $this->assertNotEmpty(GitVersion::get());
     }
 }
